7c60c0b251bbcfa6b25cfdadf0c43df558e7b929
dlipko <lipko_dima@mail.ru>
2019-02-11 17:40:17 +0300

    work with asterisk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-var ua = new SIP.UA('lipko_dima@bmstu.onsip.com');
+var config = {
+  // Replace this IP address with your Asterisk IP address
+  uri: '1060@127.0.0.1',
 
-var session = ua.invite('lipko_dima@bmstu.onsip.com');
+  // Replace this IP address with your Asterisk IP address,
+  // and replace the port with your Asterisk port from the http.conf file
+  ws_servers: 'wss://127.0.0.1:8089/ws',
+
+  // Replace this with the username from your sip.conf file
+  authorizationUser: '1060',
+
+  // Replace this with the password from your sip.conf file
+  password: 'password',
+};
+
+var ua = new SIP.UA(config);
+
+// Invite with audio only
+var session = ua.invite('1061',{
+  sessionDescriptionHandlerOptions: {
+    constraints: {
+      audio: true,
+      video: false
+    }
+  }
+});
+
 
 var remoteVideo = document.getElementById('remoteVideo');
 var localVideo = document.getElementById('localVideo');
