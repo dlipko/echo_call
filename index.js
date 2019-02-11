92b10b54a3e29e779a8999f61b7b10b552f99212
dlipko <lipko_dima@mail.ru>
2019-02-11 17:44:46 +0300

    remove hw

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile( __dirname + "/src/index.html" );
 });
 
 app.use('/', express.static(__dirname + '/src'));
